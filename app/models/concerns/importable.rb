2ab46dd885923446eb2a117f13166506de474719
Bachirdiallo <moubachir03@gmail.com>
2015-07-07 17:48:59 +0800

    Some fixes in importe file

--- app/models/concerns/importable.rb
+++ app/models/concerns/importable.rb
@@ -16,29 +16,26 @@ module Importable
 
       (data.first_row + 1..data.last_row).to_a.each do |entry|
         import_data = nil
-
-          attr_hash = self.name.constantize::IMPORT_ENABLED.select{|a, b| b[:is_identifier] == true}
-          attrs = {}
-          import_data = self.find_or_initialize_by(attrs)
-          attr_hash.each do |c, v|
-            if !mapping[c].nil? and !mapping[c].blank?
-              if !data.row(entry)[mapping[c].to_i].nil? && !data.row(entry)[mapping[c].to_i].blank?
-                if c == "user_id".to_sym
-                  user = User.find_or_create_by(:student_id => data.row(entry)[mapping[c].to_i], :email => "md@mail.com", :encrypted_password => "password")
-                  import_data[c] = user.id
-                end
-              end
-            end
-          end
-
-        import_data = self.find_or_initialize_by(attrs)
+        import_data = self.find_or_initialize_by({})
           attr_hash = self.name.constantize::IMPORT_ENABLED.select{|a, b| b[:is_identifier] == false}
           attr_hash.each do |k, v|
             if !mapping[k].nil? and !mapping[k].blank?
               if !data.row(entry)[mapping[k].to_i].nil? && !data.row(entry)[mapping[k].to_i].blank?
-                if k == "batch_id".to_sym
+                if k == "user_id".to_sym
+                  user = User.find_or_create_by(:student_id => data.row(entry)[mapping[k].to_i], :email => "md@mail.com", :encrypted_password => "password")
+                  import_data[k] = user.id
+                  import_data.save
+                elsif k == "name".to_sym
+                  puts import_data.inspect
+                  u = User.find import_data.user_id
+                  u.name = data.row(entry)[mapping[k].to_i]
+                  u.save
+                elsif k == "id_number".to_sym
+                  import_data.user.id_number = data.row(entry)[mapping[k].to_i]
+                  import_data.user.save
+                elsif k == "batch_id".to_sym
                   the_date = data.row(entry)[mapping[k].to_i].to_s
-                  the_month =  Date.strptime(the_date, '%Y-%m').month
+                  the_month = Date.strptime(the_date, '%Y-%m').month
                   the_year = Date.strptime(the_date, '%Y-%m').year
                   batch = Batch.find_or_create_by(:month => the_month, :year => the_year)
                   import_data[k] = batch.id
--- app/models/graduation.rb
+++ app/models/graduation.rb
@@ -7,10 +7,12 @@ class Graduation < ActiveRecord::Base
 
   include ::Importable
    IMPORT_ENABLED = {
-     :user_id => {exact: "Student Id", is_identifier: true},
+     :user_id => {exact: "Student Id", is_identifier: false},
+     :name => {exact: "Student Name", is_identifier: false},
+     :id_number => {exact: "Id Number", is_identifier: false},
      :batch_id => {exact: "Batch", is_identifier: false},
      :programme_id => {exact: "Programme", is_identifier: false},
      :campu_id => {exact: "Campus", is_identifier: false},
-     :faculty_id => {exact: "Faculty", is_identifier: false}
+     :faculty_id => {exact: "Faculty", is_identifier: false},
    }
 end
