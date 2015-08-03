11cf3de1c3efb15028c170286cf251c353c8219f
Bachirdiallo <moubachir03@gmail.com>
2015-08-03 10:37:15 +0800

    Home page design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,4 @@ class HomeController < ApplicationController
     @announcements = Announcement.all
     @events = Event.all
   end
-
-  def announcement
-    puts'sdfsfdsf', params[:id]
-    @a = Announcement.where(id: params[:id])
-    puts'@a', @a
-
-  end
-
-  def home_params
-    params.permit(:title, :content)
-  end
 end
