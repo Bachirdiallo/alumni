ef003f5f8bc89b1efc26a54b8a0b3c99471d5df4
Bachirdiallo <moubachir03@gmail.com>
2015-11-18 10:24:19 +0800

    Display latest articles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @announcements = Announcement.all
-    @events = Event.all
+    @announcements = Announcement.order("created_at desc").limit(3)
+    @events = Event.order("created_at desc").limit(3)
     @carousels = Carousel.where(:is_published => true)
   end
 end
