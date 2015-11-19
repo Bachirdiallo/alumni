1c39716c98b7d33699e824ced32bfd54d2f3dda9
Bachirdiallo <moubachir03@gmail.com>
2015-11-19 16:04:35 +0800

    Override slider background

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,8 @@
 //= require twitter/bootstrap/button
 //= require twitter/bootstrap/collapse
 //= require twitter/bootstrap/carousel
-
+//= require plugins/jquery/jquery.min.js
+//= require plugins/bootstrap/js/bootstrap.min.js
 //= require plugins/jquery/jquery-migrate.min.js
 //= require plugins/smoothScroll.js
 //= require plugins/parallax-slider/js/modernizr.js
