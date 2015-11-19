d46b00d6d9d5e16ca53828956abbcdcfe16ac0c8
Bachirdiallo <moubachir03@gmail.com>
2015-11-19 11:16:49 +0800

    Error checking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  #protect_from_forgery with: :null_session
-  before_filter :set_global_search_variable
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:student_id, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:student_id, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:student_id, :email, :password, :password_confirmation, :remember_me) }
   end
 
   def set_global_search_variable
