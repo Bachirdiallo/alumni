0e7f43ef6825319af5bb8acd0c975f802c8da0d6
Bachirdiallo <moubachir03@gmail.com>
2016-05-24 15:29:05 +0800

    clean up

--- app/controllers/find_users_controller.rb
+++ app/controllers/find_users_controller.rb
@@ -7,18 +7,15 @@ class FindUsersController < ApplicationController
 
   def index
      @search_results = User.ransack(params[:f])
-     @users = @search_results.result
 
      if !params[:f].nil?
        current_city = params[:f][:current_city]
+       @query = User.ransack(graduations_faculty_id_eq: params[:f][:faculty], graduations_batch_id_eq: params[:f][:batch],
+       graduations_programme_id_eq: params[:f][:programme], graduations_campu_id_eq: params[:f][:campus])
        if current_city == "1"
-         @users = @search_results.result.where(city: params[:city], country: params[:country])
-         puts '#####',@users.count
+         @users = @query.result.where(city: params[:city], country: params[:country])
        else
-         @query = User.ransack(graduations_faculty_id_eq: params[:f][:faculty], graduations_batch_id_eq: params[:f][:batch],
-         graduations_programme_id_eq: params[:f][:programme], graduations_campu_id_eq: params[:f][:campus])
          @users = @query.result
-         puts '#####',@users.count
        end
      end
 
@@ -29,25 +26,6 @@ class FindUsersController < ApplicationController
    end
 
   def map_view
-  end
-
-  def geo_locate
-    @search_results = User.ransack(params[:q])
-    #@user = @search_results.result
-  #    is_current_city = params[:q][:is_current_city]
-    @users = @search_results.result
-    puts '####', @users.count
-    if !params[:country].nil?
-      puts 'NILiiiiiiiiiiiiiiiiiiii'
-    else
-      respond_to do |format|
-        format.html{@users}
-        format.js{
-          #is_current_city = params[:q][:is_current_city]
-          @users = User.where(city: params[:city], country: params[:country])}
-          render {@users}
-      end
-    end
   end
 
   private
